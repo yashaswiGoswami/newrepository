0b46f3bb87d632c9e09b03eff42db1ab6c19bd50
niit <admin@CNF-GF-L900238>
2016-05-26 16:58:17 +0530

    this is first commit

---
+++ test1/src/firstpackage/SampleHelloClass.java
@@ -0,0 +1,14 @@
+package firstpackage;
+
+public class SampleHelloClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello this is just sample");
+		
+		System.out.println("Hello this is just sample2 change");
+		// TODO Auto-generated method stub
+
+	}
+
+}
